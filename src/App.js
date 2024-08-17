14d8f814d03d9d36985ae01b04dafe199340f25b
Rory <brown02@colostate.edu>
2024-08-17 08:27:19 -0600

    Update transaction layout and form styles
    - Adjusted CSS for form input fields to align text and datetime fields horizontally.
    - Updated transaction layout to display description and name on the left side, and price with date and time on the right.
    - Modified CSS to align date and local time on the same line within the transaction details.
    - Added spacing and alignment adjustments for improved readability and layout consistency

--- src/App.js
+++ src/App.js
@@ -17,12 +17,42 @@ function App() {
       </form>
       <div className="transactions">
         <div className="transaction">
-          <div className="left"></div>
-          <div className="name">New designer glasses</div>
-          <div className="description">Needed something for the fits</div>
+          <div className="left">
+            <div className="name">New designer glasses</div>
+            <div className="description">Needed something for the fits</div>
+          </div>
           <div className="right">
             <div className="price">$500</div>
-            <div className="datetime">2024-08-14</div>
+            <div className="datetime">
+              <div className="date">2024-08-14</div>
+              <div className="time">12:00 PM</div>
+            </div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">Concert tickets</div>
+            <div className="description">Bought for the weekend</div>
+          </div>
+          <div className="right">
+            <div className="price green">$400</div>
+            <div className="datetime">
+              <div className="date">2024-08-15</div>
+              <div className="time">6:00 PM</div>
+            </div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">Grocery shopping</div>
+            <div className="description">Weekly groceries</div>
+          </div>
+          <div className="right">
+            <div className="price red">$900</div>
+            <div className="datetime">
+              <div className="date">2024-08-16</div>
+              <div className="time">10:00 AM</div>
+            </div>
           </div>
         </div>
       </div>
@@ -30,6 +60,4 @@ function App() {
   );
 }
 
-
 export default App;
-//here
